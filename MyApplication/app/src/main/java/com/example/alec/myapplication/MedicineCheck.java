566cb08d907e77ee06c58b4e2a4098f204e9c97b
Betsy Weber <elizabeth.a.weber@vanderbilt.edu>
2016-11-01 11:14:17 -0500

    medicine activity

---
+++ MyApplication/app/src/main/java/com/example/alec/myapplication/MedicineCheck.java
@@ -0,0 +1,13 @@
+package com.example.alec.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MedicineCheck extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_medicine_check);
+    }
+}
