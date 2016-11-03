b6c596365c5ba388f7891d0a9efdeb0023ef465b
Amy Nguyen <amy.nguyen@vanderbilt.edu>
2016-11-03 12:12:06 -0500

    added signup screen

--- MyApplication/app/src/main/java/com/example/alec/myapplication/AsthmaTest.java
+++ MyApplication/app/src/main/java/com/example/alec/myapplication/AsthmaTest.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.database.sqlite.*;
 
 public class AsthmaTest extends AppCompatActivity {
 
@@ -13,17 +14,6 @@ public class AsthmaTest extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
