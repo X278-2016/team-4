64ba984e355910864e214fb06e9235a94773a512
Alec Lindner <aleclindner@gmail.com>
2016-11-15 11:58:43 -0600

    merge

--- MyApplication/app/src/main/java/com/example/alec/myapplication/CurrentStats_Screen.java
+++ MyApplication/app/src/main/java/com/example/alec/myapplication/CurrentStats_Screen.java
@@ -2,6 +2,9 @@ package com.example.alec.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class CurrentStats_Screen extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class CurrentStats_Screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_current_stats__screen);
+        final TextView aqLevel = (TextView)findViewById(R.id.AirQualityLevel_Title);
+        for(int i = 0; i<10; i++)
+        {
+            Random rand = new Random();
+            try
+            {
+                wait(3000);
+            }
+            catch (Exception e)
+            {
+
+            }
+            int val = rand.nextInt();
+            aqLevel.setText(val+"");
+        }
     }
 }
